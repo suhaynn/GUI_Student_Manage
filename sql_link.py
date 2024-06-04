296e6951519c44c9ebb570765f2396c53f307246
suhaynn <121786326+suhaynn@users.noreply.github.com>
2024-06-04 09:22:25 +0800

    Update sql_link.py

--- sql_link.py
+++ sql_link.py
@@ -8,7 +8,7 @@ db_config = {
     'host': 'localhost',
     'port': 3306,
     'user': 'root',
-    'password': '2356789mmm',
+    'password': '123456',
     'database': 'students',
     'charset': 'utf8mb4'
 }
